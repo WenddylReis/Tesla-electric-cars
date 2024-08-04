8df844afef00c2b5d2e7ef3215fb89006b1e60a8
WenddylReis <wamdreis@gmail.com>
2024-08-04 12:53:17 -0300

    Primeiro Commit - Projeto Tesla

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".formulario");
+
+function aparecerFormulario() {
+    formulario.style.display = "block";
+}
+
+function desaparecerFormulario() {
+    formulario.style.display = "none";
+}
+
